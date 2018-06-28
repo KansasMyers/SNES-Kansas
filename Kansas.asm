24b76baad043835c52d7a6e781f2bfa1011af174
KansasMyers <kansasmyers360@gmail.com>
2018-06-28 10:47:26 -0300

    Ajustes ortográficos

--- Kansas.asm
+++ Kansas.asm
@@ -19,10 +19,10 @@
      sep     #$20        ; Setando o Registrador A como 8-Bit
      lda     #%10000000  ; Forçando o VBlank a desligar a tela.
      sta     $2100
-	 ;Formato RGB em binário: 0bbbbbgg gggrrrrr - 00000000 11100000 = Verde - 01111100 00000000 - Azul
-     lda     #%00000000  ; Carregando o byte 'Baixo' da cor verde.
+     ;Formato RGB em binário: 0bbbbbgg gggrrrrr - 00000000 11100000 = Verde - 01111100 00000000 - Azul
+     lda     #%00000000  ; Carregando o byte 'Baixo' da cor azul.
      sta     $2122
-     lda     #%01111100  ; Carregando o byte 'Alto' da cor verde.
+     lda     #%01111100  ; Carregando o byte 'Alto' da cor azul.
      sta     $2122
      lda     #%00001111  ; Encerrando o VBlank e setando o brilho para 15 (100%). Bin: 00001111 = Dec: 15
      sta     $2100
